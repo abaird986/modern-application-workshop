07f2a636027f2d0336cf732c1ee40159887bf52a
Andrew Baird <andbaird@amazon.com>
2018-05-17 13:09:19 -0400

    Additional staging.

---
+++ completed-code/service/petAdoptionService.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, json, Response, request
+from flask_cors import CORS
+import mysfitsTableClient
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def errorResponse():
+    return jsonify({"message" : "Nothing here, used for health check. Try /pets instead."})
+
+@app.route("/pets")
+def getProducts():
+    
+    
+    #response = Response(open("pets-response.json").read())
+    #response.headers["Content-Type"]= "application/json"
+    
+    #return response
+    
+    filterCategory = request.args.get('filter')
+    if filterCategory:
+        filterValue = request.args.get('value')
+        queryParam = {
+            'filter': filterCategory,
+            'value': filterValue
+        }
+        serviceResponse = mysfitsTableClient.queryMysfits(queryParam)
+    else:
+        serviceResponse = mysfitsTableClient.getAllMysfits()
+    
+    flaskResponse = Response(serviceResponse)
+    flaskResponse.headers["Content-Type"] = "application/json"
+    
+    return flaskResponse
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
