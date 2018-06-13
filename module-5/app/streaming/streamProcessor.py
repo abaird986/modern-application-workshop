b081d4a7ef9619094710c91b6c9b79d9e9075e03
Andrew Baird <andbaird@amazon.com>
2018-06-12 20:39:48 -0400

    remove actual endpoint for test service.

--- module-5/app/streaming/streamProcessor.py
+++ module-5/app/streaming/streamProcessor.py
@@ -7,7 +7,7 @@ import json
 import requests
 
 def retrieveMysfit(mysfitId):
-    apiEndpoint = 'https://hryz4cuhbc.execute-api.us-east-1.amazonaws.com/prod/mysfits/' + str(mysfitId)
+    apiEndpoint = 'REPLACE_ME' + str(mysfitId)
     mysfit = requests.get(apiEndpoint).json()
     return mysfit
 
